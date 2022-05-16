eff5ab9f81a8230a9828a1a9e4a8c2f32b1f93cb
Konrad Dumin <01158980@pw.edu.pl>
2022-05-16 18:36:07 +0200

    Początki klasy HttpHandler, plik z miastami do wyboru

--- src/main/java/openweather/HttpHandler.java
+++ src/main/java/openweather/HttpHandler.java
@@ -1,5 +1,18 @@
 package openweather;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class HttpHandler {
     
+    private HttpClient client;
+
+    public HttpHandler (){
+        this.client = HttpClientBuilder.create().build();
+    }
+
+    public HttpClient getClient(){
+        return client;
+    }
+
 }
