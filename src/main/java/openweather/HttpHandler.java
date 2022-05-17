2663459d3fd510a858711bfd01e7855f72259b31
Konrad Dumin <01158980@pw.edu.pl>
2022-05-17 22:11:48 +0200

    enum Mode

--- src/main/java/openweather/HttpHandler.java
+++ src/main/java/openweather/HttpHandler.java
@@ -7,6 +7,18 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class HttpHandler {
+
+    public enum Mode {
+
+        HOURLY("current,minutely,daily,alerts"),
+        DAILY("current,minutely,hourly,alerts");
+
+        private String pattern;
+
+        Mode (String text){
+            this.pattern = text;
+        }
+    }
     
     private HttpClient client;
 
@@ -14,9 +26,12 @@ public class HttpHandler {
         this.client = HttpClientBuilder.create().build();
     }
 
-    public void makeGetRequest(String[] city){
+    public void makeGetRequest(String[] city, Mode mode, String API_key){
 
-        HttpGet getRequest = new HttpGet("placeholder");
+        HttpGet getRequest = new HttpGet
+        (String.format("https://api.openweathermap.org/data/2.5/onecall?lat=%1$s&lon=%2$s&exclude=%3$s&appid=%4$s",
+        city[1], city[2], mode.pattern, API_key));
+
         getRequest.addHeader("accept", "application/json");
         HttpResponse response;
 
